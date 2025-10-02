f6a3d076a2f7c2164915c67061ffadaa9954c745
Muntasir Hossain <126512131+muntasirhossain2003@users.noreply.github.com>
2025-10-02 13:18:13 +0600

    reels or loop Schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,15 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  userName: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  profileImage: { type: String, required: true },
-  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
-  saved:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    userName: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profileImage: { type: String },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+    saved: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
