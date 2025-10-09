6449a71b898c6e9e3fba6c9b705b5c225b744ef2
Muntasir Hossain <126512131+muntasirhossain2003@users.noreply.github.com>
2025-10-10 00:23:58 +0600

    signUP form V1

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import logo from "../assets/logo2.png";
 
 const SignUp = () => {
+  // 2.16.15
   return (
     <div className="w-full h-screen bg-gradient-to-b from-black to-gray-900 flex flex-col justify-around items-center">
       <div className="w-[90%] lg:max-w-[60%] h-[600px] bg-white rounded-2xl flex justify-center items-center overflow-hidden border-2 border-[#1a1f23]">
@@ -9,7 +10,19 @@ const SignUp = () => {
             <span>Sign Up to</span>
             <img src={logo} alt="Logo" className="w-[100px]" />
           </div>
-          <div></div>
+          <div className="relative flex items-center justify-start w-[90%] h-[50px] rounded-2xl mt-[30px] border-2 border-black">
+            <label
+              htmlFor="name"
+              className="text-gray-700 absolute left-[20px] p-[5px] bg-white text-[15px]"
+            >
+              Enter Your Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="w-[100%] h-[100%] rounded-2xl px-[20px] outline-none border-0"
+            />
+          </div>
         </div>
         <div className="md:w-[50%] h-full hidden lg:flex justify-center items-center bg-[#000000] flex-col gap-[10px] text-white text-[16px] font-semibold rounded-l-[30px] shadow-2xl shadow-black"></div>
       </div>
