3e5e5a563188294c352c1bc178286730dee006bf
Muntasir Hossain <126512131+muntasirhossain2003@users.noreply.github.com>
2025-10-10 00:18:04 +0600

    primary ui of signup page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import logo from "../assets/logo2.png";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="w-full h-screen bg-gradient-to-b from-black to-gray-900 flex flex-col justify-around items-center">
+      <div className="w-[90%] lg:max-w-[60%] h-[600px] bg-white rounded-2xl flex justify-center items-center overflow-hidden border-2 border-[#1a1f23]">
+        <div className="w-full lg:w-[50%] h-full bg-white flex flex-col items-center p-[10px] gap-[20px]">
+          <div className="flex gap-[10px] items-center text-[20px] font-semibold mt-[40px]">
+            <span>Sign Up to</span>
+            <img src={logo} alt="Logo" className="w-[100px]" />
+          </div>
+          <div></div>
+        </div>
+        <div className="md:w-[50%] h-full hidden lg:flex justify-center items-center bg-[#000000] flex-col gap-[10px] text-white text-[16px] font-semibold rounded-l-[30px] shadow-2xl shadow-black"></div>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
