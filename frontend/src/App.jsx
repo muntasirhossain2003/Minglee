6ab3730dac71b20ff4aa6291ae3ec87888984a64
Muntasir Hossain <126512131+muntasirhossain2003@users.noreply.github.com>
2025-10-20 20:07:13 +0600

    added the basic forgot password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 export const serverUrl = "http://localhost:8000";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Routes>
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
     </Routes>
   );
 };
---
+++ frontend/src/pages/ForgotPassword.jsx
@@ -0,0 +1,9 @@
+const ForgotPassword = () => {
+  return (
+    <div className="w-full h-screen bg-gradient-to-b from-black to-gray-900 flex flex-col justify-around items-center">
+      ForgotPassword
+    </div>
+  );
+};
+
+export default ForgotPassword;
