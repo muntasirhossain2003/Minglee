711d566f3157594f5317e714bd83f33622024fd5
Muntasir Hossain <126512131+muntasirhossain2003@users.noreply.github.com>
2025-10-28 22:39:12 +0600

    controller and routes for edit porfile and getProfile

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
-import { getCurrentUser, suggestedUsers } from "../controllers/user.controllers.js";
+import { editProfile, getCurrentUser, getProfile, suggestedUsers } from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.js";
 
 
 const userRouter = express.Router();
 
 userRouter.get("/current", isAuth, getCurrentUser);
 userRouter.get("/suggested", isAuth, suggestedUsers);
+userRouter.get("/getProfile/:userName", isAuth, getProfile);
+userRouter.post("/editProfile", isAuth,upload.single('profileImage'), editProfile);
 export default userRouter;
