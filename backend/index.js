2e0fce71ec7e588b01d99e389ecb4515a69fb7d9
Muntasir Hossain <126512131+muntasirhossain2003@users.noreply.github.com>
2025-10-30 22:30:05 +0600

    routes for post is done

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import express from 'express';
 import connectDB from './config/db.js';
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
+import postRouter from './routes/post.routes.js';
 
 const app = express();
 const port= process.env.PORT||5000;
@@ -19,6 +20,7 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
+app.use('/api/post', postRouter);
 
 
 
