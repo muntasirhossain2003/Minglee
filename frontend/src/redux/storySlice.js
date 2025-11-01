af712d61558865f05c309129c1816081ae9b7893
Muntasir Hossain <126512131+muntasirhossain2003@users.noreply.github.com>
2025-11-01 18:23:58 +0600

    uploading all thing is done with redux

---
+++ frontend/src/redux/storySlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const storySlice = createSlice({
+  name: "story",
+  initialState: {
+    storyData: [],
+
+  },
+  reducers: {
+    setStoryData: (state, action) => {
+      state.storyData = action.payload;
+    },
+    
+  },
+});
+
+export const { setStoryData } = storySlice.actions;
+
+export default storySlice.reducer;
